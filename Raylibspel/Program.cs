88c75fcd9d6707605ca4c4d6fc4c0160afcbadd6
AxelP312 <60465685+AxelP312@users.noreply.github.com>
2020-11-13 13:36:04 +0100

    master

---
+++ Raylibspel/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks.Dataflow;
+using System;
+using Raylib_cs;
+
+namespace Raylibspel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(800, 600, "Spelrutan");
+            Raylib.SetTargetFPS(60); 
+
+            bool gameState = true;
+            int scene = 0;
+
+            while (!Raylib.WindowShouldClose())
+            {
+                while (gameState == true)
+                {
+                    if (scene == 0)
+                    {
+                        Raylib.BeginDrawing();
+                        Raylib.ClearBackground(Color.WHITE);
+                        Raylib.DrawText("Press Enter", 100 , 250, 96, Color.BLACK);
+                        if(Raylib.IsKeyDown(KeyboardKey.KEY_ENTER))
+                        {
+                            scene = 1;
+                        }
+                        Raylib.EndDrawing();
+                        
+                    }
+                    else if (scene == 1)
+                    {
+                        Raylib.BeginDrawing();
+                        
+                        
+                        Raylib.ClearBackground(Color.ORANGE);
+
+
+                        Raylib.EndDrawing();
+                    }
+
+                }
+            }
+        }
+    }
+}
